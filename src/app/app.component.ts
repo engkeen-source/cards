706524f53409ec45d6580bea92c2224b3447da28
engkeen <71132168+engkeen-source@users.noreply.github.com>
2023-07-13 06:58:33 +0800

    publish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    { 
+      title: 'Neat Tree',
+      imageUrl: 'assets/tree.jpeg',
+      username: 'nature',
+      content: 'I saw this neat tree today'
+    },
+    { 
+      title: 'Mountain Bike',
+      imageUrl: 'assets/biking.jpeg',
+      username: 'nature 1',
+      content: 'I saw this neat tree today 1'
+    },
+    { 
+      title: 'Snowy Mountain',
+      imageUrl: 'assets/mountain.jpeg',
+      username: 'nature 2',
+      content: 'I saw this neat tree today 2'
+    }
+  ]
 }
